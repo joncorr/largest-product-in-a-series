2968903b233b6d1628b2fb3f5de394fd742edada
Jonathan <jonathan.e.corrales@gmail.com>
2017-08-12 07:35:49 -0400

    Added the initial template files.

---
+++ test/largest_product_in_a_series/core_test.clj
@@ -0,0 +1,7 @@
+(ns largest-product-in-a-series.core-test
+  (:require [clojure.test :refer :all]
+            [largest-product-in-a-series.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
