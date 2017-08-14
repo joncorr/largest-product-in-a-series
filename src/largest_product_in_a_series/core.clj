81f57470072cd7838c558195ba995bf4457c5333
Jonathan <jonathan.e.corrales@gmail.com>
2017-08-13 22:17:10 -0400

    More scratch work.

--- src/largest_product_in_a_series/core.clj
+++ src/largest_product_in_a_series/core.clj
@@ -2,19 +2,21 @@
   (:gen-class)
   (:require [clojure.string :as str]))
 
-
-(def filepath "C:\\Users\\gideon\\Documents\\GitHub\\largest-product-in-a-series\\resources\\1000digitnumber.txt")
-
 (defn get-x-digit-number [filepath]
  (.replace (slurp filepath) "\r\n" ""))
 
-(defn foo [x-digit-number]
+(defn split-into-vectors [x-digit-number]
  (str/split x-digit-number #"0"))
 
 (defn length-less-than-13? [string]
  (if (or (str/blank? string) (< (count string) 13))
    true
    false))
+
+(def greater-than-13? (complement length-less-than-13?))
+
+(defn remove-number-sequences-less-than-13 [vectors]
+ (filter greater-than-13? vectors))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- src/largest_product_in_a_series/scratch.clj
+++ src/largest_product_in_a_series/scratch.clj
@@ -1,8 +1,20 @@
 
-(:use [clojure.string :as str])
+(ns largest-product-in-a-series.core
+  (:gen-class :name scratch)
+  (:require [clojure.string :as str]))
 
 (def s "01234567890")
 
 (subs s (inc (str/index-of s \0)))
 
 (str/split s #"5")
+
+(System/getProperty "user.dir")
+
+(def filepath "resources/1000digitnumber.txt")
+
+(def x-digit-number (get-x-digit-number filepath))
+
+(def vectors (split-into-vectors x-digit-number))
+
+(map (partial count) (remove-number-sequences-less-than-13 vectors))
